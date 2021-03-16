b3c3009027c4dee5ed4861b61bb6490fbc03b357
prafullgarg11 <gargprafull55@gmail.com>
2021-03-16 11:29:10 +0530

    another change

---
+++ dictionary.py
@@ -0,0 +1,8 @@
+dic={1:"prafull",2:"garg"}           # declaration of an dictionary
+a=dic.keys()  # gives only keys
+b=dic.values()    # gives only values
+print(a,b)
+x=[12,212,32,4]
+y=["d","Dsa","dsaa","pg"]
+z=dict(zip(x,y))       # merging 2 lists one acts as keys and another as values
+print(z)
---
+++ id.type.py
@@ -0,0 +1,3 @@
+a=2
+print(id(a))       # id() gives addess of stored data
+print(type(a))     # variable is of int class .using function type(variable) we can know type of any variable
